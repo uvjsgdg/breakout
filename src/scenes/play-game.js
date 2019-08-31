d46b83875352f59a84b199365443be52eafeb3c7
Ryan Hall <ryan.hall@endurance.com>
2019-08-31 12:11:25 -0600

    row of bricks displaying

--- src/scenes/play-game.js
+++ src/scenes/play-game.js
@@ -14,13 +14,10 @@ export default class PlayGameScene extends Phaser.Scene {
 
     create () {
         let bricks = this.add.group();
-        console.log(bricks);
-        
-        // let brick = new Brick(this, 200, 200);
-        // this.add.existing(brick);
+
         for (let i = 1; i <= 11; i++) {
-            // let newBrick = new Brick(this, 64 * i, 200);
-            bricks.create(64 * i, 200, Brick);
+            let newBrick = new Brick(this, 64 * i, 200);
+            bricks.add(newBrick, true);
         }
     }
 
