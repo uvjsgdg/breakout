165850ac1e169c29cb11c6cb78cbd35650824e79
Joe Broderick <PenguinMan98@usa.net>
2019-08-31 15:40:18 -0600

    added some state bits to the paddle

--- src/sprites/paddle.js
+++ src/sprites/paddle.js
@@ -4,7 +4,33 @@ export default class Paddle extends Phaser.Physics.Arcade.Sprite {
     constructor (scene, x, y) {
         super(scene, x, y, 'spriteatlas', 'paddle');
 
-        this.frameWidth = 100;
-        this.frameHeight = 25;
+        // visual and interaction modifiers
+        this.state = {
+          onFire: false,
+          isSticky: false,
+          isDouble: false,
+          isTriple: false,
+          isIcy: false,
+          isBouncy: false,
+          color: 'default',
+          width: 100,
+          height: 25,
+        }
+
+        this.frameWidth = this.state.width;
+        this.frameHeight = this.state.height;
+
+    }
+
+    updateState( state, value ){
+      if( 'on_fire' === state && value ){
+        this.state.onFire = true;
+        // todo: do something
+        this.emit('PADDLE_ON_FIRE');
+      }else if( 'on_fire' === state && !value ){
+        this.state.onFire = false;
+        // todo: do something
+        this.emit('PADDLE_NORMAL');
+      }
     }
 }
