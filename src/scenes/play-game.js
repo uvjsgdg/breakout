5c9c4899fe255ff1aa016ce0483650389d9d3a29
Ryan Hall <ryan.hall@endurance.com>
2019-08-31 12:01:42 -0600

    adds bricks logic

--- src/scenes/play-game.js
+++ src/scenes/play-game.js
@@ -13,13 +13,15 @@ export default class PlayGameScene extends Phaser.Scene {
     }
 
     create () {
-        // let bricks = [];
-        let brick = new Brick(this, 200, 200);
-        this.add.existing(brick);
-        // for (let i = 0; i > 10; i++ ) {
-        //     let newBrick = new Brick(this, 200, 200);
-        //     bricks.push(newBrick);
-        // }
+        let bricks = this.add.group();
+        console.log(bricks);
+        
+        // let brick = new Brick(this, 200, 200);
+        // this.add.existing(brick);
+        for (let i = 1; i <= 11; i++) {
+            // let newBrick = new Brick(this, 64 * i, 200);
+            bricks.create(64 * i, 200, Brick);
+        }
     }
 
     update () {
