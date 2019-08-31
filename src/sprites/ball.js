5c2ea415d9a60e368684cd97f3807ff9b1955f47
Daniel Fackrell <unlearned@gmail.com>
2019-08-31 13:26:03 -0600

    Inherited from a physics-enabled class

--- src/sprites/ball.js
+++ src/sprites/ball.js
@@ -1,6 +1,6 @@
 // Ball Sprite is concerned about the display of the ball
 // import config from '../config/game';
-export default class Ball extends Phaser.GameObjects.Sprite {
+export default class Ball extends Phaser.Physics.Arcade.Sprite {
     constructor (scene, x, y) {
         super(scene, x, y, 'spriteatlas', 'ball');
     }
--- src/sprites/brick.js
+++ src/sprites/brick.js
@@ -1,6 +1,6 @@
 // The Brick Sprite is concerned with display of one brick
 import config from '../config/game';
-export default class Brick extends Phaser.GameObjects.Sprite {
+export default class Brick extends Phaser.Physics.Arcade.Sprite {
   constructor (scene, x,  y) {
     super(scene, x, y, config.spriteAtlas.key, 'brick')
   }
