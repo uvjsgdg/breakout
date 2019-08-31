ba273cea2dfb0184b8bcca6dfa037a5a91e32ff6
Ryan Hall <ryan.hall@endurance.com>
2019-08-31 10:58:13 -0600

    adds brick sprite and displays brick

--- src/scenes/play-game.js
+++ src/scenes/play-game.js
@@ -1,4 +1,5 @@
 // The Play Game scene is concerned about activating the level sub-scene and ui sub-scene
+import Brick from '../sprites/brick';
 export default class PlayGameScene extends Phaser.Scene {
     constructor (config, key = 'PlayGame') {
         super({ key: key });
@@ -12,6 +13,13 @@ export default class PlayGameScene extends Phaser.Scene {
     }
 
     create () {
+        // let bricks = [];
+        let brick = new Brick(this, 200, 200);
+        this.add.existing(brick);
+        // for (let i = 0; i > 10; i++ ) {
+        //     let newBrick = new Brick(this, 200, 200);
+        //     bricks.push(newBrick);
+        // }
     }
 
     update () {
