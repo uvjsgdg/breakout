90c577341c405dd427bd1f2707e6c9f29284ab98
Daniel Fackrell <unlearned@gmail.com>
2019-08-31 13:28:59 -0600

    Inherited from a physics-enabled class

--- src/sprites/paddle.js
+++ src/sprites/paddle.js
@@ -1,6 +1,6 @@
 // Paddle Sprite is concerned about the display of the paddle
 
-export default class Paddle extends Phaser.GameObjects.Sprite {
+export default class Paddle extends Phaser.Physics.Arcade.Sprite {
     constructor (scene, x, y) {
         super(scene, x, y, 'spriteatlas', 'paddle');
     }
