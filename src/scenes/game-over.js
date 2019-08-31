578b19ddb34dacb5871ebfe2f92e695a3c990126
gaudeon <gaudeon@cpan.org>
2019-08-31 16:24:24 -0600

    added score to game over scene

--- src/scenes/game-over.js
+++ src/scenes/game-over.js
@@ -1,4 +1,6 @@
 // The Play Game scene is concerned about activating the level sub-scene and ui sub-scene
+import config from '../config/game';
+
 export default class GameOverScene extends Phaser.Scene {
     constructor (config, key = 'GameOver') {
         super({ key: key });
@@ -12,8 +14,16 @@ export default class GameOverScene extends Phaser.Scene {
     }
 
     create () {
-        let text = this.add.text(400, 300, 'Game Over!', { fontFamily: 'Arial, Sans', fontSize: '32px'});
-        text.setOrigin(0.5, 0.5);
+        let gameOverText = this.add.text(400, 200, 'Game Over!', { fontFamily: 'Arial, Sans', fontSize: '64px'});
+        gameOverText.setOrigin(0.5, 0.5);
+
+        let yourScoreText = this.add.text(400, 300, 'Your Score Is...', { fontFamily: 'Arial, Sans', fontSize: '32px'});
+        yourScoreText.setOrigin(0.5, 0.5);
+
+        let mainGameScene = this.scene.get('PlayGame');
+
+        let scoreText = this.add.text(400, 350, mainGameScene.data.get(config.data.playerScoreKey), { fontFamily: 'Arial, Sans', fontSize: '32px'});
+        scoreText.setOrigin(0.5, 0.5)
     }
 
     update () {
