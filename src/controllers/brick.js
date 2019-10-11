337e9e1e4159c4063d98de73edad3823243fa1fe
dizzydogg <jeffthetenor@yahoo.com>
2019-10-11 14:06:09 -0600

    tint the bricks

--- src/controllers/brick.js
+++ src/controllers/brick.js
@@ -2,6 +2,10 @@
 export default class BrickController extends Phaser.Events.EventEmitter {
     onBrickCollision(ball, brick) {
         brick.lives--;
+        var MINTINT = 0x666666;
+        var tintFactor = Math.round(0x99 * brick.lives / brick.maxLives);
+        brick.setTint(MINTINT + 0x010101 * tintFactor);
+        console.log(brick.tintTopLeft);
         if (brick.lives == 0) {
             brick.destroy(true);
             this.emit('BrickDestroyed');
