d1917ed08224060164f61cee531749b237d2f989
gaudeon <gaudeon@cpan.org>
2019-12-07 15:35:30 -0700

    explosive balls break breaks not matter the lives on the brick

--- src/sprites/powerup.js
+++ src/sprites/powerup.js
@@ -3,7 +3,6 @@ import config from '../config/game';
 export default class PowerupSprite extends Phaser.Physics.Arcade.Sprite {
     constructor (scene, x, y, type = 'ball') {
         // ball equals extra life powerup for now
-        console.log(scene);
         super(scene, x, y, config.spriteAtlas.key, type);
 
         if (type == 'ball') {
